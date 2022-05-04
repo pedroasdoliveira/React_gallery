95266c4c0e5fb0d4cc4a4a41aa54fdd77146002a
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-05-04 18:57:51 -0300

    preparando tela de atualização

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,10 @@
 import "./Navbar.css";
+import { ActionMode } from "constants/index.js";
 import icon from "../../assets/icons/galery-icon.svg";
-import {GrAdd} from 'react-icons/gr';
+import { GrAdd } from "react-icons/gr";
+import { RiImageEditLine } from "react-icons/ri";
 
-const Navbar = ({createCard}) => {
+const Navbar = ({ createCard, updateCard, mode }) => {
   return (
     <header className="Header">
       <div className="Header__row">
@@ -10,9 +12,24 @@ const Navbar = ({createCard}) => {
           <h1 className="Description__title">Galeria</h1>
         </div>
         <div className="Galeries">
-          <button className="Create" type="button" onClick={() => createCard()}>
+          <button 
+            className={`Update Galery ${
+              mode === ActionMode.ATUALIZAR && "Card--Ativo"
+            }`}
+            type="button"
+            onClick={() => updateCard()}
+          > 
+            <RiImageEditLine />
+          </button>
+          {/* button update */}
+          <button
+            className="Create Galery"
+            type="button"
+            onClick={() => createCard()}
+          >
             <GrAdd />
           </button>
+          {/* button create */}
           <div className="Images">
             <img className="Images__icon" src={icon} alt="Icone de galerias" />
           </div>
--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,25 +1,41 @@
 import { useState } from "react";
+import { ActionMode } from "constants/index.js";
 import GaleriaLista from "../../components/GaleriaLista/GaleriaLista.jsx";
 import "./Home.css";
 import Navbar from "components/Navbar/Navbar.jsx";
 import AddGaleriaModal from "components/AddGaleriaModal/AddGaleriaModal.jsx";
 import Footer from "components/Footer/Footer.jsx";
 
-const Home = ({galeriaCriada}) => {
+const Home = ({ galeriaCriada }) => {
   const [canShowAddGaleriaModal, setCanShowAddGaleriaModal] = useState(false);
 
   const [galeryForAdd, setGaleryForAdd] = useState();
 
+  const [modoAtual, setModoAtual] = useState(ActionMode.NORMAL);
+
+  const handleActions = (action) => {
+    const novaAcao = modoAtual === action ? ActionMode.NORMAL : action;
+    setModoAtual(novaAcao);
+  };
+
   return (
     <div className="Home">
-      <Navbar createCard={() => setCanShowAddGaleriaModal(true)} />
+      <Navbar
+        mode={modoAtual}
+        createCard={() => setCanShowAddGaleriaModal(true)}
+        updateCard={() => handleActions(ActionMode.ATUALIZAR)}
+      />
+      {/* Navbar */}
       <div className="Home__container">
         <GaleriaLista galeriaCriada={galeriaCriada} />
-        {
-          canShowAddGaleriaModal && (<AddGaleriaModal closeModal={() => setCanShowAddGaleriaModal(false)} 
-          onCreateGaleria = {(galeria) => setGaleryForAdd(galeria)} />)
-        }
+        {canShowAddGaleriaModal && (
+          <AddGaleriaModal
+            closeModal={() => setCanShowAddGaleriaModal(false)}
+            onCreateGaleria={(galeria) => setGaleryForAdd(galeria)}
+          />
+        )}
       </div>
+      {/* GaleriaLista */}
       <div className="Home__footer">
         <Footer />
       </div>
